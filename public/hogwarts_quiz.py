5cdbe881cb1ee12e7ef47c4ca3ba0572dc3340ce
Floroush <florian.araujogruen@gmail.com>
2023-05-28 20:41:28 +0200

    updated

--- public/hogwarts_quiz.py
+++ public/hogwarts_quiz.py
@@ -2,13 +2,13 @@ print("Welcome, young wizard/witch!")
 
 favorite_color = input("What's your favorite color? Red, blue, green or yellow? ")
 
-if favorite_color == "red":
+if favorite_color == "red" or "Red":
     print("Griffindor or Ravenclaw")
-elif favorite_color == "blue":
+elif favorite_color == "blue" or "Blue":
     print("Ravenclaw or Slytherin")
-elif favorite_color == "green":
+elif favorite_color == "green" or "Green":
     print("Slytherin or Hufflepuff")
-elif favorite_color == "yellow":
+elif favorite_color == "yellow" or "Yellow":
     print("Hufflepuff or Gryffindor")
 else:
     print("Please choose a favorite color!")
